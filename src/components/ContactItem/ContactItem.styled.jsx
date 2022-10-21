e3abb7f622c25fddebd354c93b3faf7a65d864f5
YuriiLeo <yuriileontenko@gmail.com>
2022-10-21 16:01:07 +0300

    add styles

--- src/components/ContactItem/ContactItem.styled.jsx
+++ src/components/ContactItem/ContactItem.styled.jsx
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
 export const Item = styled.li`
     display: flex;
     align-items: center;  
+    justify-content: space-between;
 `;
 
 export const Button = styled.button`
