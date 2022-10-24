6ec32460d7c31783a1626ed5d63abb8692de7bf4
YuriiLeo <yuriileontenko@gmail.com>
2022-10-24 03:10:36 +0300

    add provider &persistGate

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  App  from 'components/App/App'; 
 import './index.css';
+import { Provider } from "react-redux";
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <PersistGate loading={null} persistor={persistor} >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
+ </React.StrictMode> 
+  
 );
